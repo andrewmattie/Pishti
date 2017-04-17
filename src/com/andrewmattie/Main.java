0588e07a5903533ac75f90d8c55c8a6aaae04940
Andrew Mattie <andrewmattie@gmail.com>
2017-04-17 19:22:01 -0400

    Added basic structure

---
+++ src/com/andrewmattie/Main.java
@@ -0,0 +1,8 @@
+package com.andrewmattie;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/andrewmattie/objects/Deck.java
@@ -0,0 +1,7 @@
+package com.andrewmattie.objects;
+
+/**
+ * Created by andrewmattie on 4/17/17.
+ */
+public class Deck {
+}
