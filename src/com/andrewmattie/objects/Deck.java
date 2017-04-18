743e1fbba8d490f48a594a5ab7df3093db30d683
Andrew Mattie <andrewmattie@gmail.com>
2017-04-18 11:50:01 -0400

    Added methods to Deck

--- src/com/andrewmattie/objects/Deck.java
+++ src/com/andrewmattie/objects/Deck.java
@@ -1,7 +1,45 @@
 package com.andrewmattie.objects;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by andrewmattie on 4/17/17.
  */
 public class Deck {
+
+    private ArrayList<Integer> arrayList;
+
+    public Deck(ArrayList<Integer> arrayList) {
+        this.arrayList = arrayList;
+        shuffle();
+    }
+
+    public void shuffle() {
+        arrayList.clear();
+
+        for (int i = 1; i <= 54; i++) {
+            arrayList.add(i);
+        }
+
+        Collections.shuffle(arrayList);
+    }
+
+    public ArrayList<Integer> dealCards() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            list.add(dealCard());
+        }
+        return list;
+    }
+
+    public int dealCard() {
+        int card = arrayList.get(0);
+        arrayList.remove(0);
+        return card;
+    }
+
+    public ArrayList<Integer> getCardList() {
+        return arrayList;
+    }
 }
