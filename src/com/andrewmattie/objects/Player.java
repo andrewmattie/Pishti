bf457b6ab759b138b85b1cea4e1f0e66a001957c
Andrew Mattie <andrewmattie@gmail.com>
2017-04-18 12:11:10 -0400

    Basic Player

--- src/com/andrewmattie/objects/Player.java
+++ src/com/andrewmattie/objects/Player.java
@@ -1,7 +1,23 @@
 package com.andrewmattie.objects;
 
+import java.util.ArrayList;
+
 /**
  * Created by andrewmattie on 4/17/17.
  */
 public class Player {
+
+    private ArrayList<Integer> playerCardsList;
+
+    public Player(ArrayList<Integer> playerCardsList) {
+        this.playerCardsList = playerCardsList;
+    }
+
+    public ArrayList<Integer> getPlayerCardsList() {
+        return playerCardsList;
+    }
+
+    public void setPlayerCardsList(ArrayList<Integer> playerCardsList) {
+        this.playerCardsList = playerCardsList;
+    }
 }
